8d438c0b1abc0c6748113f4e66c9d6aeb828b99c
卿云辉 <qingyunhui@zuozh.com>
2016-11-03 18:11:37 +0800

    properties加解密

--- cn.com.yunzhushui.movie.service/src/main/java/cn/com/yunzhushui/movie/apply/CipherPropertyConfigurer.java
+++ cn.com.yunzhushui.movie.service/src/main/java/cn/com/yunzhushui/movie/apply/CipherPropertyConfigurer.java
@@ -3,8 +3,16 @@ package cn.com.yunzhushui.movie.apply;
 import java.util.Properties;
 
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanInitializationException;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+
+import cn.com.yunzhushui.movie.constant.PropertyConstant;
+import qing.yun.hui.common.enums.ICommonEnum;
+import qing.yun.hui.common.enums.PropertiesEnum;
+import qing.yun.hui.common.utils.EnumUtil;
+import qing.yun.hui.common.utils.RSAUtil;
+import qing.yun.hui.common.utils.StringUtil;
 
 /***
  ** @category 请用一句话来描述其用途...
@@ -20,25 +28,36 @@ public class CipherPropertyConfigurer extends PropertyPlaceholderConfigurer{
 	
 	@Override
 	protected void processProperties(ConfigurableListableBeanFactory beanFactoryToProcess, Properties props)throws BeansException {
-		/* try {
-           String username = props.getProperty(PropertyConstant.JDBC_DRUID_USER);
-           if (username != null) {
-               props.setProperty(PropertyConstant.JDBC_DRUID_USER, DesEncrypt.Decrypt(username, DesEncrypt.hex2byte(key)));
-           }
-           
-           String password = props.getProperty(PropertyConstant.JDBC_DRUID_PASSWORD);
-           if (password != null) {
-               props.setProperty(PropertyConstant.JDBC_DRUID_PASSWORD, DesEncrypt.Decrypt(password, DesEncrypt.hex2byte(key)));
-           }
-           
-           String url = props.getProperty(PropertyConstant.JDBC_DRUID_URL);
-           if (url != null) {
-               props.setProperty(PropertyConstant.JDBC_DRUID_URL, DesEncrypt.Decrypt(url, DesEncrypt.hex2byte(key)));
-           }
+		 try {
+			 resetData(props, PropertyConstant.PRIVATE_KEY);
            super.processProperties(beanFactoryToProcess, props);
        } catch (Exception e) {
            e.printStackTrace();
            throw new BeanInitializationException(e.getMessage());
-       }*/
+       }
+	}
+	
+	/**
+	 * 对指定数据使用私钥进行解密
+	 * @param props 加密的后Properties(源)
+	 * @param privateKey 待解密的私钥
+	 * */
+	protected void resetData(Properties props,String privateKey) throws Exception{
+		Object[] objs=EnumUtil.getEnumValues(PropertiesEnum.class.getName());
+		if(null!=objs&& objs.length>0){
+			for(Object obj:objs){
+				ICommonEnum ico=(ICommonEnum) obj;
+				String key=ico.getName();
+				String encryptionData= props.getProperty(key);
+				if(!StringUtil.isEmpty(encryptionData)){
+					try {
+						props.setProperty(key, RSAUtil.decryptionByPrivateKey(privateKey, encryptionData));
+					} catch (Exception e) {
+						//TODO 如果解密出现错误，那么忽略继续下一步..
+						continue;
+					}
+				}
+			}
+		}
 	}
 }
