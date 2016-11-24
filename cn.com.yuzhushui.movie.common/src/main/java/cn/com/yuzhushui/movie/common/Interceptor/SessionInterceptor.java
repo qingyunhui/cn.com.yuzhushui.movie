55365a7a6ce0844cc107edbea2725e34df4537d6
qingyunhui <280672161@qq.com>
2016-11-24 22:58:30 +0800

    do

---
+++ cn.com.yuzhushui.movie.common/src/main/java/cn/com/yuzhushui/movie/common/Interceptor/SessionInterceptor.java
@@ -0,0 +1,10 @@
+package cn.com.yuzhushui.movie.common.Interceptor;
+/***
+ ** @category 拦截器
+ ** @author qing.yunhui
+ ** @email: 280672161@qq.com
+ ** @createTime: 2016年11月24日下午11:33:33
+ **/
+public interface SessionInterceptor {
+
+}
