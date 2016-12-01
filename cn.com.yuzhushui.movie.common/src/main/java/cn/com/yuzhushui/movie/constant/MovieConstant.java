9d0a562c7b6a227fb2249f3711c74953faa0b61f
卿云辉 <qingyunhui@zuozh.com>
2016-12-01 17:54:50 +0800

    update code

--- cn.com.yuzhushui.movie.common/src/main/java/cn/com/yuzhushui/movie/constant/MovieConstant.java
+++ cn.com.yuzhushui.movie.common/src/main/java/cn/com/yuzhushui/movie/constant/MovieConstant.java
@@ -11,9 +11,27 @@ public class MovieConstant {
 	public static final String SESSION_INFO="SessionInfo";
 
 	/**消息*/
-	public static final String MESSAGES="messages";
+	public static final String MESSAGES_INFO="messages";
 	
 	/**登陆次数标识*/
-	public static final String  LOGON_COUNT="logonCount";
+	public static final String  LOGON_COUNT_INFO="logonCount";
+	
+	/**登陆次数**/
+	public static final int LOGON_COUNTS=3;
+	
+	/**锁定时间（单位:分钟）*/
+	public static final int LOCK_TIME=30;
+	
+	/**项目的顶级域名(子域名，二级域名都可以访问到，比如:abc.smiles8.com,top.smiles8.com)**/
+	public static final String DOMAIN="smiles8.com";
+	
+	/**项目名称*/
+	public static final String PROJECT_NAME="MOVIE";
+	
+	/**连接符*/
+	public static final String JOINT="&=yzs=?";
+	
+	/**根目录*/
+	public static final String ROOT_PATH = "/";
 	
 }
--- cn.com.yuzhushui.movie.web/src/main/java/cn/com/yuzhushui/movie/app/web/action/AppMainAction.java
+++ cn.com.yuzhushui.movie.web/src/main/java/cn/com/yuzhushui/movie/app/web/action/AppMainAction.java
@@ -17,6 +17,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import qing.yun.hui.common.utils.CookieUtil;
+import qing.yun.hui.common.utils.EnumUtil;
+import qing.yun.hui.common.utils.GenerateRuleUtil;
+import qing.yun.hui.common.utils.MD5Util;
+import qing.yun.hui.common.utils.StringUtil;
+import qing.yun.hui.common.utils.ValidateUtil;
 import cn.com.yuzhushui.movie.cache.ShardedJedisCached;
 import cn.com.yuzhushui.movie.common.bean.LogParameter;
 import cn.com.yuzhushui.movie.common.bean.SessionInfo;
@@ -27,10 +33,8 @@ import cn.com.yuzhushui.movie.sys.biz.entity.SysAccount;
 import cn.com.yuzhushui.movie.sys.biz.entity.SysUser;
 import cn.com.yuzhushui.movie.sys.biz.service.SysAccountService;
 import cn.com.yuzhushui.movie.sys.biz.service.SysUserService;
-import qing.yun.hui.common.utils.CookieUtil;
-import qing.yun.hui.common.utils.MD5Util;
-import qing.yun.hui.common.utils.StringUtil;
-import qing.yun.hui.common.utils.ValidateUtil;
+
+import com.alibaba.fastjson.JSONObject;
 
 /***
  ** @category 请用一句话来描述其用途...
@@ -101,13 +105,40 @@ public class AppMainAction {
 	
 	/**登陆*/
 	@RequestMapping(value = "/doLogin", method = RequestMethod.POST)
-	public ModelAndView doLogin(LogParameter logParam, HttpSession session,RedirectAttributes attributes) {
+	public ModelAndView doLogin(HttpServletRequest request,HttpServletResponse response, HttpSession session,LogParameter logParam,RedirectAttributes attributes) {
 		ModelAndView modeView = new ModelAndView("redirect:" + ACTION_PATH + "/login.htm");
 		Map<String, Object> map = new HashMap<String, Object>();
+		//@1.非空校验
 		if(StringUtil.isEmpty(logParam.getAccounts())){
-			modeView.addObject(MovieConstant.MESSAGES,"请输入手机号/邮箱/账号!");
+			String message="请输入手机号/邮箱/账号!";
+			modeView.addObject(MovieConstant.MESSAGES_INFO,message);
+			logger.error("===========>"+message+"<===========");
 			return modeView;
 		}
+		if(StringUtil.isEmpty(logParam.getPasswords())){
+			String message="请输入登陆密码!";
+			modeView.addObject(MovieConstant.MESSAGES_INFO,message);
+			logger.error("===========>"+message+"<===========");
+			return modeView;
+		}
+		//@2.用户登陆密码输入错误次数校验
+		String userLogCount=logParam.getAccounts()+"_"+MovieConstant.MESSAGES_INFO;//当前用户登陆次数
+		String logCountStr=shardedJedisCached.get(userLogCount);
+		if(!StringUtil.isEmpty(logCountStr)){
+			int logCount=Integer.parseInt(logCountStr);
+			if(logCount > MovieConstant.LOGON_COUNTS){
+				StringBuilder sb=new StringBuilder();
+				sb.append("密码输入错误超过");
+				sb.append(MovieConstant.LOGON_COUNTS);
+				sb.append("次，请");
+				sb.append(MovieConstant.LOCK_TIME);
+				sb.append("分钟后重试。");
+				modeView.addObject(MovieConstant.MESSAGES_INFO,sb.toString());
+				logger.error("===========>"+sb.toString()+"<===========");
+				return modeView;
+			}
+		}
+		//@3.登陆类型校验
 		Integer loginType=loginType(logParam.getAccounts());
 		if(loginType.equals(LoginType.MOBILEPHONE_TYPE)){
 			map.put("mobilephone", logParam.getAccounts());
@@ -117,6 +148,7 @@ public class AppMainAction {
 			map.put("account", logParam.getAccounts());
 		}
 		map.put("password", MD5Util.getMD5Encryption(logParam.getPasswords()));
+		//@4.判断数据库中是否存在该用户
 		List<SysAccount> accounts=sysAccountService.query(map);
 		if (accounts.size() == 1) {
 			SysAccount account = accounts.get(0);
@@ -124,22 +156,31 @@ public class AppMainAction {
 			userMap.put("accountId", account.getAccountId());
 			List<SysUser> users=sysUserService.query(userMap);
 			if(users.size()==1){
-				if(account.getStatus()!=SysAccountEnum.STATUS.AUDIT_SUCCESS.getValue()){
-					attributes.addFlashAttribute(MovieConstant.MESSAGES, "登录失败，用户已被禁用或者没有注册！");
-					return modeView;
-				}else{
+				if(account.getStatus().intValue()==SysAccountEnum.STATUS.AUDIT_SUCCESS.getValue()){
+					//登陆成功-把用户存储到cookie中。
+					modeView.setViewName("redirect:/main/myMain.htm");
 					SysUser sysUser=users.get(0);
 					SessionInfo sessionInfo = new SessionInfo();
 					sessionInfo.setSysUser(sysUser);
 					sessionInfo.setObject(account);
-					session.setAttribute(MovieConstant.SESSION_INFO, sessionInfo);// 登录成功，获取用户信息，并存入session
-					modeView = new ModelAndView("redirect:/main/myMain.htm");
+//					session.setAttribute(MovieConstant.SESSION_INFO, sessionInfo);// 登录成功，获取用户信息，并存入session
+					String sessionId = GenerateRuleUtil.generateUnique(MovieConstant.JOINT, MovieConstant.PROJECT_NAME);
+					//设置cookie有效期
+					int indate=2*60*60;//设置cookie有效为:2个小时
+					String userInfoJson=JSONObject.toJSONString(sessionInfo);
+					CookieUtil.setCookie(request, response, MovieConstant.SESSION_INFO, sessionId, MovieConstant.DOMAIN, MovieConstant.ROOT_PATH,indate);
+					shardedJedisCached.set(MovieConstant.SESSION_INFO, userInfoJson, indate);
+					return modeView;
+				}else{
+					String enumName=EnumUtil.getNameByValue(SysAccountEnum.STATUS.class, account.getStatus());
+					attributes.addFlashAttribute(MovieConstant.MESSAGES_INFO, "登录失败，该用户已"+enumName+"!");
+					return modeView;
 				}
 			}else{
-				attributes.addFlashAttribute(MovieConstant.MESSAGES, "登录失败，用户名或密码错误！");
+				attributes.addFlashAttribute(MovieConstant.MESSAGES_INFO, "登录失败，用户名或密码错误！");
 			}
 		} else {
-			attributes.addFlashAttribute(MovieConstant.MESSAGES, "登录失败，用户名或密码错误！");
+			attributes.addFlashAttribute(MovieConstant.MESSAGES_INFO, "登录失败，用户名或密码错误！");
 		}
 		return modeView;
 	}
