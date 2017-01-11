c46ebbba702bc44489164aa9eae581063c59c8c3
卿云辉 <qingyunhui@zuozh.com>
2017-01-11 09:22:26 +0800

    update sendEmail

--- cn.com.yuzhushui.movie.service/src/main/java/cn/com/yuzhushui/movie/job/EmailNotificationJob.java
+++ cn.com.yuzhushui.movie.service/src/main/java/cn/com/yuzhushui/movie/job/EmailNotificationJob.java
@@ -4,6 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import qing.yun.hui.common.struct.weather.ResponseData;
+import qing.yun.hui.common.struct.weather.WeatherUtil;
+import qing.yun.hui.common.struct.weather.WetherConstant;
 import qing.yun.hui.mailtool.MailTool;
 import cn.com.yuzhushui.schedule.job.core.Job;
 
@@ -19,7 +22,6 @@ import com.alibaba.fastjson.JSONObject;
 public class EmailNotificationJob implements Job{
 
 	Logger logger=LoggerFactory.getLogger(EmailNotificationJob.class);
-	
 	
 	/*
 	245634919@qq.com
@@ -41,9 +43,7 @@ public class EmailNotificationJob implements Job{
 	3233574897@qq.com
 	*/
 	
-	
 	public String execute(String parameters) {
-		
 		logger.info("==================hello word==================");
 		logger.info("==================>>execute successEmailNotificationJob<==================");
 		logger.info("==================good by.==================");
@@ -54,11 +54,23 @@ public class EmailNotificationJob implements Job{
 	}	
 	
 	private void sendEmail(){
+		String httpUrl=WetherConstant.httpUrl;
+		String location="杭州";
+		String output="xml";
+		String ak=WetherConstant.ak;
+		String subject =location+"未来四天，天气预报状态。";
+		String content =null;
 		try {
+			content=WeatherUtil.callBaiduWeatherByResponse(httpUrl,location,output,ak);
 			String[] emails = new String[] { "qingyunhui@zuozh.com","627699563@qq.com","280672161@qq.com"};
-			MailTool.sendTextMail(null,"Hello word!","说点什么呢？",emails);
+			MailTool.sendTextMail(null,subject,content,emails);
 		} catch (Exception e) {
 			logger.error("=======>邮件发送失败，异常原因：{}",new Object[]{JSONObject.toJSONString(e)});
 		}
+	}
+	
+	public static void main(String[] args){
+		EmailNotificationJob job=new EmailNotificationJob();
+		job.sendEmail();
 	}
 }
